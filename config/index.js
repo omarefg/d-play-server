5c1296b63264174863ccff84a90ff33a092c80a4
Omar Flores <omarefg92@gmail.com>
2019-10-18 21:08:26 -0500

    36 create user routes (#39)
    * Qa (#33)
    
    * added mongo lib to project (#3)
    
    * added user service (#2)
    
    * Qa (#35)
    
    * added mongo lib to project (#3)
    
    * added user service (#2)
    
    * 6 create express server (#34)
    
    * Qa (#33)
    
    * added mongo lib to project (#3)
    
    * added user service (#2)
    
    * created express server
    
    * started user routes
    
    * added routes to server
    
    * created user routes

--- config/index.js
+++ config/index.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const config = {
-    nodeEnv: process.env.NODE_ENV,
-    port: process.env.PORT || '3300',
-    cors: process.env.CORS,
-    dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD,
-    dbHost: process.env.DB_HOST,
-    dbName: process.env.DB_NAME
-}
+  nodeEnv: process.env.NODE_ENV,
+  port: process.env.PORT || '3300',
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+};
 
-module.exports = { config }
+module.exports = { config };
--- services/UserService.js
+++ services/UserService.js
@@ -1,40 +1,43 @@
-const MongoLib = require('../lib/MongoLib')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const MongoLib = require('../lib/MongoLib');
 
 class UserService {
-    constructor () {
-        this.collection = 'users'
-        this.mongoDB = new MongoLib()
+  constructor() {
+    this.collection = 'users';
+    this.mongoDB = new MongoLib();
+  }
+
+  async getUser({ email }) {
+    const [user] = await this.mongoDB.getAll(this.collection, { email });
+    return user;
+  }
+
+  async createUser({ user }) {
+    const {
+      name, email, password, birthdate,
+    } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUserId = await this.mongoDB.create(this.collection, {
+      name,
+      email,
+      password: hashedPassword,
+      birthdate,
+    });
+
+    return createdUserId;
+  }
+
+  async getOrCreateUser({ user }) {
+    const queriedUser = await this.getUser({ email: user.email });
+
+    if (queriedUser) {
+      return queriedUser;
     }
 
-    async getUser ({ email }) {
-        const [ user ] = await this.mongoDB.getAll(this.collection, { email })
-        return user
-    }
-
-    async createUser ({ user }) {
-        const { name, email, password } = user
-        const hashedPassword = await bcrypt.hash(password, 10)
-
-        const createdUserId = await this.mongoDB.create(this.collection, {
-            name,
-            email,
-            password: hashedPassword
-        })
-
-        return createdUserId
-    }
-
-    async getOrCreateUser({ user }) {
-        const queriedUser = await this.getUser({ email: user.email })
-
-        if (queriedUser) {
-            return queriedUser
-        }
-
-        await this.createUser({ user })
-        return this.getUser({ email: user.email })
-    }
+    await this.createUser({ user });
+    return this.getUser({ email: user.email });
+  }
 }
 
-module.exports = UserService
+module.exports = UserService;
