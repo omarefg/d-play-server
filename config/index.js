92b2370fce909b725960d2f5955b125b471c6b02
Omar Flores <omarefg92@gmail.com>
2019-10-18 19:39:41 -0500

    Qa (#33)
    * added mongo lib to project (#3)
    
    * added user service (#2)

---
+++ config/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const config = {
+    nodeEnv: process.env.NODE_ENV,
+    port: process.env.PORT || '3300',
+    cors: process.env.CORS,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME
+}
+
+module.exports = { config }
