e7b14b62d62994c6b2ba29c732ba31f043db9962
Omar Flores Grimontt <omar.flores@3dves.com>
2019-10-18 20:23:04 -0500

    added routes to server

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const debug = require('debug')('app:server')
 
 const app = express()
 const { config } = require('./config')
+const { authApi } = require('./routes')
+
+const {
+    logErrors,
+    errorHandler,
+    wrapError,
+    notFoundHandler
+} = require('./utils/middlewares/error-handlers')
 
 // Body Parser
 app.use(express.json())
 app.use(helmet())
 
 // Routes
+authApi(app)
+
+// Catch 404
+app.use(notFoundHandler)
+
+// Errors Middlewares
+app.use(logErrors)
+app.use(wrapError)
+app.use(errorHandler)
 
 app.listen(config.port, () => {
     debug(`Listening http://localhost:${config.port}`)
