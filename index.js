3675bf7b81d11d7f1178bce47f82759787651a54
Omar Flores Grimontt <omar.flores@3dves.com>
2019-11-19 06:57:46 -0500

    added logs to server initialization

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ if (isDev) {
         cert: fs.readFileSync('/etc/letsencrypt/live/dplay.cf/fullchain.pem'),
     }, app).listen(port, (error) => {
         if (error) {
-            debug(error);
+            console.log(error);
         }
-        debug(`Listening ${apiUrlWithoutPort}:${port}`);
+        console.log(`Listening ${apiUrlWithoutPort}:${port}`);
     });
 }
